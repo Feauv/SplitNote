0f39a998d8c5d970e7154b6096f34a1220879558
Nathan Banderas <nathancbanderas@gmail.com>
2018-10-12 21:27:07 -0500

    Initial Commit

---
+++ SplitNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SplitNote
+//
+//  Created by Nathaniel Banderas on 10/12/18.
+//  Copyright © 2018 teamSplitNote. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
